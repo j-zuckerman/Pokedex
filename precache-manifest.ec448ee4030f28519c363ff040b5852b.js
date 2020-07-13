3aee98674433fe21ecc747f41278a121a7b52f7f
James Zuckerman <jzuckerman4@gmail.com>
2020-07-13 15:03:25 -0700

    Updates

---
+++ precache-manifest.ec448ee4030f28519c363ff040b5852b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e66edbb6f0941810d7f3aab6f23665",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "b7a6f6b3678e8f64277d",
+    "url": "/Pokedex/static/css/main.ee29b836.chunk.css"
+  },
+  {
+    "revision": "a1e7edf6da8557765d2c",
+    "url": "/Pokedex/static/js/2.d372c798.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokedex/static/js/2.d372c798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a6f6b3678e8f64277d",
+    "url": "/Pokedex/static/js/main.03efd6a1.chunk.js"
+  },
+  {
+    "revision": "9fb1140b615de1adf3f4",
+    "url": "/Pokedex/static/js/runtime-main.e9efafc6.js"
+  }
+]);
